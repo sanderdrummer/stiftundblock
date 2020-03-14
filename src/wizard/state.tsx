93c1ae44a57295983dc9c614ad8d7c9be11cd97f
Tobias Pickel <tobias.pickel@xing.com>
2020-03-14 10:43:38 +0100

    feat(wizard): add wizard

---
+++ src/wizard/state.tsx
@@ -0,0 +1,75 @@
+import { useReducer } from "react";
+
+export type Players = string[];
+export type Score = number[];
+
+export type State = {
+  players: Players;
+  rounds: Score[];
+};
+
+export type WizardActions =
+  | { type: "SetPlayers"; payload: string[] }
+  | { type: "NextRound"; scores: number[] }
+  | { type: "Reset"; state: State };
+
+export const wizardReducer = (state: State, action: WizardActions) => {
+  switch (action.type) {
+    case "NextRound":
+      return {
+        ...state,
+        rounds: [...state.rounds, action.scores]
+      };
+    case "SetPlayers":
+      return {
+        ...state,
+        players: action.payload
+      };
+    case "Reset":
+      return { ...action.state };
+    default:
+      return state;
+  }
+};
+
+export const useWizardState = () => {
+  return useReducer(wizardReducer, {
+    players: [],
+    rounds: []
+  });
+};
+
+const getAmountOfRounds = (playerCount = 3) => {
+  return 60 / playerCount;
+};
+
+export const getScore = (index: number, scores: Score[]) => {
+  return scores.reduce((playerScore, score) => {
+    return playerScore + score[index];
+  }, 0);
+};
+export const getPlayers = (state: State) => state.players;
+export const getPlayerNames = (state: Players) => Object.keys(state);
+export const getPlayerCount = (state: Players) => getPlayerNames(state).length;
+export const getGameStats = (state: State) => {
+  const players = getPlayers(state);
+  const playerCount = getPlayerCount(players);
+  const rounds = getAmountOfRounds(playerCount);
+  const cardAmount = state.rounds.length + 1;
+  const mapPlayerFunction = (player: any, index: number) => ({
+    name: player,
+    score: getScore(index, state.rounds)
+  });
+  const leaderBoard = players
+    .map(mapPlayerFunction)
+    .sort((a, b) => b.score - a.score);
+  const dealerIndex = cardAmount % playerCount;
+  const dealer = players[dealerIndex];
+
+  return {
+    rounds,
+    cardAmount,
+    leaderBoard,
+    dealer
+  };
+};
---
+++ src/wizard/wizard-game.tsx
@@ -0,0 +1,181 @@
+import React from "react";
+
+import {
+  Box,
+  Button,
+  Typography,
+  TextField,
+  Card,
+  CardContent,
+  CardHeader,
+  Avatar,
+  Chip
+} from "@material-ui/core";
+
+import {
+  useWizardState,
+  getPlayerCount,
+  getGameStats,
+  State,
+  Players
+} from "./state";
+
+const KEY = "WIZARD";
+
+export const WizardPage = () => {
+  const [state, dispatch] = useWizardState();
+  const playerCount = getPlayerCount(state.players);
+
+  React.useEffect(() => {
+    const json = window.localStorage.getItem(KEY);
+    try {
+      const state = JSON.parse(json || "");
+      dispatch({ type: "Reset", state });
+    } catch {}
+  }, [dispatch]);
+
+  React.useEffect(() => {
+    window.localStorage.setItem(KEY, JSON.stringify(state));
+  }, [state]);
+
+  return (
+    <>
+      <Box mt={3}>
+        <Typography>WIZARD</Typography>
+      </Box>
+      {playerCount === 0 ? (
+        <PlayersForm
+          onSubmit={names => dispatch({ type: "SetPlayers", payload: names })}
+        />
+      ) : (
+        <Game
+          addScore={scores => dispatch({ type: "NextRound", scores })}
+          state={state}
+        />
+      )}
+      <Box mt={6}>
+        <Button
+          onClick={() =>
+            dispatch({ type: "Reset", state: { players: [], rounds: [] } })
+          }
+          variant="contained"
+          color="primary"
+        >
+          neues spiel
+        </Button>
+      </Box>
+    </>
+  );
+};
+
+const RoundForm: React.FC<{
+  onSubmit: (scores: number[]) => void;
+  players: Players;
+}> = ({ players, onSubmit }) => {
+  return (
+    <form
+      autoComplete="off"
+      onSubmit={e => {
+        e.preventDefault();
+        const scores: number[] = players.map(player =>
+          Number(e.currentTarget[player].value)
+        );
+        onSubmit(scores);
+        e.currentTarget.reset();
+      }}
+    >
+      <Typography>Punkte:</Typography>
+      {players.map(player => (
+        <TextField
+          key={player}
+          label={player}
+          placeholder="Punkte"
+          required
+          fullWidth
+          margin="normal"
+          type="number"
+          name={player}
+        />
+      ))}
+      <Card>
+        <Button variant="contained" type="submit">
+          nächste runde
+        </Button>
+      </Card>
+    </form>
+  );
+};
+
+const Game: React.FC<{
+  state: State;
+  addScore: (scores: number[]) => void;
+}> = ({ state, addScore }) => {
+  const stats = getGameStats(state);
+  return (
+    <>
+      <Card>
+        <CardHeader
+          title={`Runde ${stats.cardAmount} von ${stats.rounds}`}
+          subheader={`${stats.cardAmount} Karten werden verteilt Es gibt ${stats.dealer}`}
+        ></CardHeader>
+        <CardContent>
+          <RoundForm onSubmit={addScore} players={state.players} />
+          <Box m={5}>
+            <Typography>Siegertreppchen</Typography>
+            <Box mt={2} display="flex" justifyContent="space-between">
+              {stats.leaderBoard.map((player, index) => (
+                <Chip
+                  color="primary"
+                  label={
+                    <>
+                      {player.name} {player.score}
+                    </>
+                  }
+                  avatar={<Avatar>{index + 1}</Avatar>}
+                />
+              ))}
+            </Box>
+          </Box>
+        </CardContent>
+      </Card>
+    </>
+  );
+};
+
+const PlayersForm: React.FC<{
+  onSubmit: (names: string[]) => void;
+  count?: number;
+}> = ({ onSubmit, count = 6 }) => {
+  const maybePlayers = Array(count).fill("");
+  return (
+    <form
+      onSubmit={e => {
+        e.preventDefault();
+        const playerNames = maybePlayers
+          .map(
+            //@ts-ignore
+            (_, index) => e.target[`playerName${index + 1}`].value
+          )
+          .filter(name => name.length);
+        onSubmit(playerNames);
+      }}
+    >
+      {maybePlayers.map((_, index) => (
+        <TextField
+          required={index < 3}
+          key={index}
+          autoComplete="off"
+          label={`Spieler ${index + 1}`}
+          name={`playerName${index + 1}`}
+          placeholder="Name"
+          margin="normal"
+          type="text"
+          fullWidth
+        />
+      ))}
+      <Button variant="contained" color="primary" type="submit">
+        spiel starten
+      </Button>
+    </form>
+  );
+};
